1bafe43248c3832f5ceca12c2daedd274f46d3fb
Raphael Bourassa <bourassa.raphael@outlook.com>
2020-05-10 18:02:20 -0400

    Adding the possibility to add a link to the source code.

--- custom-posts/web.php
+++ custom-posts/web.php
@@ -10,10 +10,18 @@ require_once(__DIR__."/../components/photos_gallery.php");
             <div class="col-lg-12">
             <table class="table light">
                 <tbody>
-                    <tr>
-                        <th scope="row">Lien vers le site</th>
-                        <td><?php echo '<a href="'.get_field('lien_vers_le_site').'" target="_blank">'.get_field('lien_vers_le_site').'</a>' ?></td>
-                    </tr>
+                    <?php if(get_field('lien_vers_le_site')){ ?>
+                        <tr>
+                            <th scope="row">Lien vers le site</th>
+                            <td><?php echo '<a href="'.get_field('lien_vers_le_site').'" target="_blank">'.get_field('lien_vers_le_site').'</a>' ?></td>
+                        </tr>
+                    <?php } ?>
+                    <?php if(get_field('lien_vers_le_code')){ ?>
+                        <tr>
+                            <th scope="row">Lien vers le code</th>
+                            <td><?php echo '<a href="'.get_field('lien_vers_le_code').'" target="_blank">'.get_field('lien_vers_le_code').'</a>' ?></td>
+                        </tr>
+                    <?php } ?>
                     <tr>
                         <th scope="row">Logiciel(s) utilisé</th>
                         <td><?php the_field('logiciels_utilise') ?></td>
